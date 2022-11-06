904617aa9e87b400b899e0f262739cb5949d41bc
Ahranna Araujo <ahranna@ahrannas-mbp.lan>
2022-11-06 18:12:42 +0000

    post de /sign-up e post de /tweets feito.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 
+const app = express();
+app.use(cors())
+app.use(express.json())
 
 const users = [
     {
@@ -27,28 +30,43 @@ const tweets = [
 ]
 
 
-
-const app = express();
-app.use(cors())
-
-
 app.get("/tweets", (request, response) => {
-     const resposta = tweets.map((tweet) => {
+    const resposta = tweets.map((tweet) => {
         const avatar = users.find((user) => user.username === tweet.username).avatar
         return {
             username: tweet.username,
             avatar: avatar,
             tweet: tweet.tweet
         }
-     })
+    })
 
     response.send(resposta);
 })
 
 
-app.post = (request, response) => {
-    users.push(request.data)
-}
+app.post("/sign-up", (request, response) => {
+    console.log(request.body)
+    const newUser = {
+
+        username: request.body.username,
+        avatar: request.body.avatar
+
+    }
+
+    users.push(newUser)
+    response.send("Ok")
+
+})
+
+app.post("/tweets", (request, response) => {
+    const newTweet = {
+        username: request.body.username,
+        tweet: request.body.tweet
+    }
+
+    tweets.push(newTweet)
+    response.send("Ok")
+})
 
 app.listen(5000, () => {
     console.log("Server running in port: 5000")
