d30a971351024397dae6c13aedf275841445185d
Ahranna Araujo <ahranna@ahrannas-mbp.lan>
2022-11-07 11:17:31 +0000

    projeto concluido

--- index.js
+++ index.js
@@ -5,29 +5,8 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-const users = [
-    {
-        username: "lulamolusco",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    },
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
-    },
-
-]
-
-const tweets = [
-    {
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-
-    {
-        username: "lulamolusco",
-        tweet: "aprendendo back-end"
-    }
-]
+const users = []
+const tweets = []
 
 
 app.get("/tweets", (request, response) => {
@@ -39,11 +18,8 @@ app.get("/tweets", (request, response) => {
             tweet: tweet.tweet
         }
     })
-
     const ultimosTweets = resposta.slice(-10);
     response.send(ultimosTweets);
-
-
 })
 
 
